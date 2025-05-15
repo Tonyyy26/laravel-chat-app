0cd22716c88651b2ccab77411ad1ac93445f8345
Anthony Pablo <apablo_dev@pisopay.com.ph>
2025-05-15 18:06:49 +0800

    Add: Initial setup for chat app

---
+++ app/Livewire/Chat/Chat.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Chat;
+
+use Livewire\Component;
+
+class Chat extends Component
+{
+    public function render()
+    {
+        return view('livewire.chat.chat')
+        ->layout('components.layouts.app');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Livewire\Chat\Chat;
+use App\Livewire\Chat\Index;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/chat', Index::class)->name('chat.index');
+Route::get('/chat/{query}', Chat::class)->name('chat');
